0064e4400241d27d168192c4d2077f10062e47b5
k-kyg <kygkd38@gmail.com>
2019-10-12 01:14:18 +0900

    もっと短く@PHP

--- main.php
+++ main.php
@@ -1,9 +1,5 @@
 <?php
 $list = [];
-for($i = 0; $i < 100; ++$i){
-	array_push($list, $i);
-}
-foreach($list as $i){
-	echo $i."\n";
-}
+for($i = 0; $i < 100; ++$i) array_push($list, $i);
+foreach($list as $i) echo $i."\n";
 ?>
