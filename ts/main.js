e32819811d00250179cc47961b9b9018bb132f5f
k-kyg <kygkd38@gmail.com>
2019-10-12 01:11:40 +0900

    よりTypeScriptっぽくしたつもり

--- ts/main.js
+++ ts/main.js
@@ -1,8 +1,7 @@
 "use strict";
 var list = new Array();
-for (var i = 0; i < 100; ++i) {
+for (var i = 0; i < 100; ++i)
     list.push(i);
-}
 for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
     var i = list_1[_i];
     console.log(i);
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,3 @@
-const list: number[] = new Array<number>();
+const list: Array<number> = new Array<number>();
 for (let i: number = 0; i < 100; ++i) list.push(i);
 for (let i of list) console.log(i);
